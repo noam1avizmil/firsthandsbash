1d2bad0dfa88725b84329955f03497d8ef044363
noam1avizmil <158157537+noam1avizmil@users.noreply.github.com>
2025-11-06 22:53:23 +0200

    Fix loop conditions and error messages in script

--- calculatePayment.sh
+++ calculatePayment.sh
@@ -7,7 +7,7 @@ regex_numbers="^[0-9]+([.][0-9]+)?$"
 # checking if the parameters are populated using condition with -z 
 if [[  -z ${arr_of_params[0]} ||  -z ${arr_of_params[${#arr_of_params[@]}-1]} ]] ; then
     empty_params=0;
-    for((i = 0; i <${arr_of_params[${#arr_of_params[@]}-1]}; i++)); do
+    for((i = 0; i<$((${#arr_of_params[@]}-1)); i++)); do
         if [[ -z ${arr_of_params[i]} ]] ; then
            empty_params=$i
         fi
@@ -17,11 +17,11 @@ if [[  -z ${arr_of_params[0]} ||  -z ${arr_of_params[${#arr_of_params[@]}-1]} ]]
 
 #if we dont exit we can continue with the compution 
 elif  [[ ! ${arr_of_params[${#arr_of_params[@]}-1]} =~ $regex_numbers ]] ; then
-    >&2 echo "Not a valid number : ${arr_of_params[${#arr_of_params[@]}-1]}"
+    >&2 echo "Not a valid number : ${#arr_of_params[@]}"
     error=1
 
 else 
-    for((i=0; i<${#arr_of_params[@]}-1 ; i++)) ; do
+    for((i=0; i<$((${#arr_of_params[@]}-1)) ; i++)) ; do
        if [[ ! -f ${arr_of_params[i]} ]] ; then
           >&2 echo "File does not exist : ${arr_of_params[i]}"
           error=1
@@ -33,7 +33,7 @@ if  [[ $error -eq 1 ]] ; then
 
 elif [[ $error -eq 0 ]] ; then 
     sum=0
-    for((i=0; i<${#arr_of_params[@]}-1 ; i++)) ; do
+    for((i=0; i<$((${#arr_of_params[@]}-1)) ; i++)) ; do
         sum=$((sum + $( cat ${arr_of_params[i]} | grep -Eo '[0-9]+(\.[0-9]+)?' )))
         # we are using piping to first read the file and then look for the numbers with grep -E for regex and o for only matching numbers that mathc, int or maybe also float. 
         # the (.)? is for the option that we may have point and numbers(float)
